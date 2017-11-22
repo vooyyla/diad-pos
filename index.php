70be002c892303cfc9e5d24eef03019056f68033
vooyyla <vooyyla@users.noreply.github.com>
2017-11-23 02:01:11 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <title>Collapsible sidebar using Bootstrap 3</title>
+	<!-- jQuery CDN -->
+         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+         <!-- Bootstrap Js CDN -->
+         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+         <!-- Bootstrap CSS CDN -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- Our Custom CSS -->
+        <link rel="stylesheet" href="inxludes/css/index.css">
+    </head>
+    <body>
+
+
+
+        <div class="wrapper">
+            <!-- Sidebar Holder -->
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <h3>Bootstrap Sidebar</h3>
+                    <strong>BS</strong>
+                </div>
+
+                <ul class="list-unstyled components">
+                    <li>
+                        <a >
+                            <i class="glyphicon glyphicon-home"></i>
+                            Scan
+                        </a>                        
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="glyphicon glyphicon-shopping-cart"></i>
+                            Items
+                        </a>                                              
+                    </li>
+                    <li>
+                        <a href="" id="sales">
+                            <i class="glyphicon glyphicon-tasks"></i>
+                            Sales
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="glyphicon glyphicon-th"></i>
+                            Report
+                        </a>
+                    </li>
+			<li>
+                        <a href="setting.php">
+                            <i class="glyphicon glyphicon-wrench"></i>
+                            Setting
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="glyphicon glyphicon-send"></i>
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+
+               </nav>
+
+            <!-- Page Content Holder -->
+            <div id="content">
+
+                <nav class="navbar navbar-inverse">
+                    <div class="container-fluid">
+
+                        <div class="navbar-header">
+                            <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn">
+                                <i class="glyphicon glyphicon-align-left"></i>
+                                <span>Toggle Sidebar</span>
+                            </button>
+                        </div>
+
+                        
+                    </div>
+                </nav>            
+
+                <div class="line"></div>
+		<div id="maindiv">
+		</div>
+
+            </div>
+        </div>
+
+
+
+
+
+        
+
+         <script type="text/javascript">
+             $(document).ready(function () {
+                 $('#sidebarCollapse').on('click', function () {
+                     $('#sidebar').toggleClass('active');
+                 });
+             });
+         </script>
+	<script>
+    $(function() {
+      $("#sales").click(function(evt) {
+         $("#maindiv").load("sales.php")
+         evt.preventDefault();
+      })
+    })
+</script>
+    </body>
+</html>
+
---
+++ login.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link rel="stylesheet" href="includes/css/bootstrap.min.css">
+	<link rel="stylesheet" href="includes/css/main.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+	<script src="includes/css/bootstrap.min.css.js"></script> 
+	
+	<title> DiademTech</title>
+</head>
+<body>
+	
+	<form class="form" role="form">
+		<div class="form-group">
+			<label for="exampleInputEmail1">Email address</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
+		<div class="form-group">
+			<label for="exampleInputPassword1">Password</label>
+			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		</div>
+		<div class="checkbox">
+			<label>
+			<input type="checkbox">Remember me</label>
+		</div>
+		<button type="login" class="btn btn-primary">Login</button> <a href="#forgotpass.php">Forgot password</a>
+		<br></br><button type="signup" class="btn btn-success">SignUp</button>
+	</form>
+</body>
+</html>
